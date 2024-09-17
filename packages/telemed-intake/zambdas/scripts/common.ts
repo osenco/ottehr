c0e403215d32c4a04869cb93e33ed7b53ae34b5f
Mauko Maunde <maukoese@gmail.com>
2024-09-17 14:39:18 +0300

    Add env credentials

--- packages/telemed-intake/zambdas/scripts/common.ts
+++ packages/telemed-intake/zambdas/scripts/common.ts
@@ -1,13 +1,14 @@
-import devConfig from '../.env/dev.json';
 // import testingConfig from '../.env/testing.json';
 import { FhirClient, ZambdaClient } from '@zapehr/sdk';
+
+import devConfig from '../.env/dev.json';
 import { getM2MClientToken } from '../src/shared';
 
 export const performEffectWithEnvFile = async (callback: (config: any) => void) => {
   const env = process.argv[2];
 
   switch (env) {
-    case 'dev':
+    case 'development':
       await callback(devConfig);
       break;
     // case 'testing':
